ae5d3fc58e56fc6c8c38037b2c4020c1bee4bb53
SeijiIto <ito.seiji21@gmail.com>
2019-04-26 16:50:34 +0900

    add a function that page move

---
+++ js/pagemove.js
@@ -0,0 +1,24 @@
+﻿
+var pageLinkSet = function () {
+    // リンク先のファイル名を作成
+    var pagetype = filename.replace(/[0-9]/g, '').split('.')[0];
+    var next;
+    var prev;
+    var prevpageNo = parseInt(pageno) - 1;
+    var nextpageNo = parseInt(pageno) + 1;
+    if (pagetype == "index") {
+        prev = "journal" + prevpageNo.toString() + ".html"
+        next = "journal" + pageno + ".html"
+    } else {
+        prev = "index" + pageno + ".html"
+        next = "index" + nextpageNo + ".html"
+    }
+
+    // リンク設定
+    document.addEventListener("DOMContentLoaded", function () {
+        document.getElementById("prev").setAttribute("href", prev);
+        document.getElementById("next").setAttribute("href", next);
+    });
+};
+
+pageLinkSet();
---
+++ js/pageno.js
@@ -0,0 +1,9 @@
+﻿
+var filepath = location.pathname;
+var pathinfo = filepath.split('/');
+var filename = pathinfo.pop();
+
+//gは全ての文字を対象にする
+//[]の中に^(キャレット)を入れると指定した文字を含まないという意味になる
+var pageno = filename.replace(/[^0-9]/g, '');
+document.title = "BOTW-travel journal day" + pageno + "-"
